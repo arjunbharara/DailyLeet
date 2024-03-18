625c0ae98ab21273f4ae924ab00f36bb31d8e643
arjun bharara <92726043+arjunbharara@users.noreply.github.com>
2024-03-18 12:44:55 +0530

    Time: 51 ms (98.20%) | Memory: 69.1 MB (36.85%) - LeetSync

--- LeetcodeProblems/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ LeetcodeProblems/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -4,11 +4,11 @@ class Solution {
             return 0;
         }
 
-        Arrays.sort(points,(a,b) ->a[1]-b[1]);
+        Arrays.sort(points,(a,b) ->Integer.compare(a[1],b[1]));
         int arrowCount=1;
         int target=points[0][1];
         for(int i=1;i<points.length;i++){
-            if(target>=points[i][0] && target<=points[i][1]){
+            if(target>=points[i][0]){
                 continue;
             }
             arrowCount++;
