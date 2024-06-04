3f93a1a328288ba5e933500fb3ca34a3ed04de4a
arjun bharara <92726043+arjunbharara@users.noreply.github.com>
2024-06-05 00:19:42 +0530

    Time: 1 ms (100.00%) | Memory: 41.7 MB (46.48%) - LeetSync

--- LeetcodeProblems/409-longest-palindrome/longest-palindrome.java
+++ LeetcodeProblems/409-longest-palindrome/longest-palindrome.java
@@ -1,18 +1,27 @@
-class Solution {
+
+    class Solution {
     public int longestPalindrome(String s) {
-         if(s==null || s.length()==0) return 0;
-        HashSet<Character> hs = new HashSet<Character>();
-        int count = 0;
-        for(int i=0; i<s.length(); i++){
-            if(hs.contains(s.charAt(i))){
-                hs.remove(s.charAt(i));
-                count++;
-            }else{
-                hs.add(s.charAt(i));
+        int[] array = new int[58];
+        int count = 0,odd = 0;
+
+        for(char each : s.toCharArray()){
+            array[each - 'A']++;
+        }
+
+        for(int each : array){
+            if(each % 2 == 0 && each != 0){
+                count += each;
+            }else if(each % 2 == 1 && each != 1){
+                count += each -1; 
+                odd = 1;
+            }else if(each == 1){
+                odd = 1;
             }
         }
-        if(!hs.isEmpty()) return count*2+1;
-        return count*2;
 
+        return count + odd;
+
+
+        // return array[25];
     }
 }
